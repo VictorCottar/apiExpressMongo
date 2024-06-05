aedef23b10cf4e45bc9eb1c865ffd5910e4655ed
VictorCottar <victorcottarms@outlook.com>
2024-06-05 18:09:13 -0300

    Juntando entidades

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,8 +3,8 @@ import livros from './livrosRoutes.js'
 import autores from './autoresRoutes.js'
 
 const routes = (app) => {
-    app.use(express.json(), livros)
-    app.use(express.json(), autores)
+    
+    app.use(express.json(), livros, autores)
 }
 
 export default routes;
